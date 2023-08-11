6e2d4aff61c7bbd4bdf318c6642e2270410803fb
Rahul-Webenetic <rahul.chaurasiya@webenetic.com>
2023-08-11 18:27:24 +0530

    More on req query

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,41 +1,41 @@
-require("dotenv").config();
+require('dotenv').config()
 // async errors
+require('express-async-errors')
+const express = require('express')
+const app = express()
 
-const express = require("express");
-const app = express();
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 
-const connectDB = require("./db/connect");
-const productsRouter = require("./routes/products");
-
-const notFundMiddleWare = require("./middleware/not-found");
-const errorMiddleWare = require("./middleware/error-handler");
+const notFundMiddleWare = require('./middleware/not-found')
+const errorMiddleWare = require('./middleware/error-handler')
 
 // middleware
-app.use(express.json());
+app.use(express.json())
 
 // routes
 
-app.get("/", (req, res) => {
-  res.send("<h1>Store API</h1> <a href='/api/v1/products'>Products</a>");
-});
+app.get('/', (req, res) => {
+  res.send("<h1>Store API</h1> <a href='/api/v1/products'>Products</a>")
+})
 
-app.use("/api/v1/products", productsRouter);
+app.use('/api/v1/products', productsRouter)
 
 // Products routes
 
-app.use(notFundMiddleWare);
-app.use(errorMiddleWare);
+app.use(notFundMiddleWare)
+app.use(errorMiddleWare)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000
 
 const start = async () => {
   try {
     // connect db
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI)
     app.listen(PORT, () => {
-      console.log("Server is listening on PORT " + PORT + "...");
-    });
+      console.log('Server is listening on PORT ' + PORT + '...')
+    })
   } catch (error) {}
-};
+}
 
-start();
+start()
--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,11 +1,35 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "Products testing route" });
-};
+  const search = 'ab'
+  const products = await Product.find({}).sort('-name price')
+  res.status(200).json({ products, nbhits: products.length })
+}
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Products testing" });
-};
+  const { featured, company, name, sort } = req.query //query is handy
+  const queryObject = {}
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }
+  }
+  // console.log(queryObject)
+  let result = Product.find(queryObject) // Since filter needs an object we create an object beforehand
+  if (sort) {
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createdAt') // default sort value
+  }
+  const products = await result
+  res.status(200).json({ products, nbhits: products.length })
+}
 
 module.exports = {
   getAllProducts,
   getAllProductsStatic,
-};
+}
