c75f180600dcfe9c4507a2875291f8732d4096e4
Rahul <therahulchaurasia@gmail.com>
2023-09-02 22:38:59 +0530

    Started new project

--- 06.5-jobster-api/starter/app.js
+++ 06.5-jobster-api/starter/app.js
@@ -1,64 +1,52 @@
-require('dotenv').config();
-require('express-async-errors');
+require('dotenv').config()
+require('express-async-errors')
 
 // extra security packages
-const helmet = require('helmet');
-const cors = require('cors');
-const xss = require('xss-clean');
-const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet')
+const xss = require('xss-clean')
 
-// Swagger
-const swaggerUI = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const express = require('express')
+const app = express()
+const path = require('path')
 
-const express = require('express');
-const app = express();
-
-const connectDB = require('./db/connect');
-const authenticateUser = require('./middleware/authentication');
+const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
 // routers
-const authRouter = require('./routes/auth');
-const jobsRouter = require('./routes/jobs');
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const { __esModule } = require('xss-clean/lib/xss')
 
-app.set('trust proxy', 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
-);
-app.use(express.json());
-app.use(helmet());
-app.use(cors());
-app.use(xss());
-
-app.get('/', (req, res) => {
-  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-});
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+console.log(path.resolve(__dirname, './client/build', 'index.html'))
+app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.json())
+app.use(helmet())
+app.use(xss())
 
 // routes
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
-const port = process.env.PORT || 5000;
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
-    );
+    )
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-start();
+start()
