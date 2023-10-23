b552250c8cb732add2c8085e05815caec580f72f
Rahul Chaurasia <therahulchaurasia@gmail.com>
2023-10-23 17:05:15 +0530

    Added reviews

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -7,12 +7,14 @@ const connectDB = require('./db/connect')
 const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
 const productRouter = require('./routes/productRoutes')
+const reviewRouter = require('./routes/reviewRoutes')
 
 //express
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser') // Use it parse cookies
+const fileUpload = require('express-fileupload')
 
 //Middleware
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -21,6 +23,8 @@ const notFoundMiddleware = require('./middleware/not-found')
 app.use(morgan('tiny'))
 app.use(express.json()) //To access the json data received in the body during api calls
 app.use(cookieParser(process.env.JWT_SECRET)) // We need this package to parse cookies and we send the secret to sign the cookie.
+app.use(express.static('./public'))
+app.use(fileUpload())
 
 app.get('/', (req, res) => {
   res.send('e-commerce-api')
@@ -34,6 +38,7 @@ app.get('/api/v1', (req, res) => {
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productRouter)
+app.use('/api/v1/reviews', reviewRouter)
 
 app.get('/', (req, res) => {
   res.status(200).send('home')
@@ -42,7 +47,7 @@ app.get('/', (req, res) => {
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL)
--- 10-e-commerce-api/starter/controllers/productController.js
+++ 10-e-commerce-api/starter/controllers/productController.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require('http-status-codes')
 const Product = require('../models/Products')
 const CustomError = require('../errors/index')
+const path = require('path')
 const createProduct = async (req, res) => {
   req.body.user = req.user.userId
   const product = await Product.create(req.body)
@@ -49,7 +50,25 @@ const deleteProduct = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: `Success! Product Removed` })
 }
 const uploadImage = async (req, res) => {
-  res.status(StatusCodes.OK).send('Route to upload Image')
+  console.log(req.files);
+  if(!req.files)
+  {
+    throw new CustomError.BadRequestError('No file uploaded')
+  }
+  const productImage = req.files.image
+  if(!productImage.mimetype.startsWith('image'))
+  {
+    throw new CustomError.BadRequestError('Please upload image')
+  }
+  
+  const maxSize = 1024 * 1024;
+  if(productImage.size > maxSize)
+  {
+    throw new CustomError.BadRequestError('Please upload image smaller than 1MB')
+  }
+  const imagePath = path.join(__dirname,'../public/uploads/' + `${productImage.name}`)
+  await productImage.mv(imagePath);
+  res.status(StatusCodes.OK).json({image:`/uploads/${productImage.name}`})
 }
 
 module.exports = {
